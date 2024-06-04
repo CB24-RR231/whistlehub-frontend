26e421fda143cff97576942db552a07d45f68b16
Lotaf <lotaf@Lotaf-MacBook-2.local>
2024-06-05 06:55:39 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from './components/Footer'
+import Navigation from './components/Navigation'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Navigation />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
