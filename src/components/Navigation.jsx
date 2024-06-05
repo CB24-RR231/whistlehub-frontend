d493a8f4c18534b1aa4234bd124140fd9ad512b8
adhimukti <adhimukti46@gmail.com>
2024-06-05 14:06:02 +0700

    navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,22 +5,14 @@ function Navigation() {
 		<div className='navigation'>
 			<Link to='/' className='logo-nav'>WhistleHub</Link>
 			<nav>
-<<<<<<< HEAD
-				<Link to='#'>Pages 1</Link>
-=======
 				<Link to='/menu'>Laporan</Link>
 				<Link to='/about'>About</Link>
 				<Link to='/contact'>Contact</Link>
->>>>>>> a8e0001d443176b4d461f6296f905e3528f3b53b
 				<button
 					type='button'
 					className='sign-in-button'
 				>
-<<<<<<< HEAD
-					Sign in
-=======
 					Login
->>>>>>> a8e0001d443176b4d461f6296f905e3528f3b53b
 				</button>
 			</nav>
 		</div>
